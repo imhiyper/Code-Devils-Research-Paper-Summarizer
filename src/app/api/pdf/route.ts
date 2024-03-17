fe91da075afc7dc10447f669fc7fe23faf3af518
Frank J L <67761168+frankjlin16@users.noreply.github.com>
2024-03-16 22:10:58 -0700

    feat: update api for pdf summarization

--- src/app/api/pdf/route.ts
+++ src/app/api/pdf/route.ts
@@ -1,23 +1,37 @@
 // TODO: We need a decided on LLM + .env for it (plus 'ai' module for streaming)
 
-import OpenAI from 'openai'
-import { OpenAIStream, StreamingTextResponse } from 'ai'
+import OpenAI from "openai";
+// import { OpenAIStream, StreamingTextResponse } from 'ai'
 
 const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
-})
+});
 
-export const runtime = 'edge'
+export const runtime = "edge";
 
 export async function POST(req: Request) {
-  const { messages } = await req.json()
-  const response = await openai.chat.completions.create({
-    model: 'gpt-3.5-turbo',
+  const extractedText = await req.text();
+  const completion = await openai.chat.completions.create({
+    model: "gpt-3.5-turbo",
     stream: true,
-    messages,
-  })
+    messages: [
+      {
+        role: "system",
+        content:
+          "You are a text summarizer. You will summarize the provided text.",
+      },
+      {
+        role: "user",
+        content: extractedText,
+      },
+    ],
+  });
 
-  const stream = OpenAIStream(response)
+  for await (const chunk of completion) {
+    console.log(chunk.choices[0].delta.content);
+  };
 
-  return new StreamingTextResponse(stream)
+  // const stream = OpenAIStream(response)
+
+  // return new StreamingTextResponse(stream)
 }
